03c0e6678bf0d74e18586e66f233474984333a79
NoOne223 <namle392001@gmail.com>
2024-07-10 16:11:06 +0700

    update

--- HTML/Fashion/assets/js/main.js
+++ HTML/Fashion/assets/js/main.js
@@ -6,26 +6,58 @@ $(document).ready(function(){
         fade: true,
         speed: 2000,
         autoplay: true,
-        autoplaySpeed: 5000,
+        autoplaySpeed: 3000,
         pauseOnHover: false,
+    });
+    // AOS //
+    AOS.init({
+        duration: 1200,
+        once: true,
     });
 
     //RUNNING NUMBER
-    $('.running-number').each(function() {
-        var $this = $(this);
-        var target = parseInt($this.attr('data-target'));
+    function runCounter($element) {
+        var target = parseInt($element.attr('data-target'));
         var count = 0;
         var increment = target / 100;
         function updateCounter() {
-            if(count < target) {
+            if (count < target) {
                 count += increment;
-                $this.text(Math.ceil(count).toLocaleString() + '+');
+                $element.text(Math.ceil(count).toLocaleString() + '+');
                 setTimeout(updateCounter, 30);
             } else {
-                $this.text(target.toLocaleString() + '+');
+                $element.text(target.toLocaleString() + '+');
             }
         }
         updateCounter();
+    }
+    $('.running-number').each(function() {
+        var $this = $(this);
+        $this.waypoint(function() {
+            runCounter($this);
+            this.destroy();
+        }, {
+            offset: '90%'
+        });
+    });
+
+    //DROP-BRANCHES
+    $('.drop-branches').click(function() {
+        var branch = $(this).next('.branch');
+        $('.branch').not(branch).slideUp();
+        branch.slideToggle();
+        if (branch.is(':visible')) {
+            var offsetTop = branch.offset().top;
+            $('html, body').animate({
+                scrollTop: offsetTop
+            }, 500);
+        }
+    });
+    $(document).click(function(e) {
+        var container = $(".branch-contact");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $('.branch').slideUp();
+        }
     });
 
 });
